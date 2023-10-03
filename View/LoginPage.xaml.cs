8ced2199ea88fdf8e78259a0b0d6e10e70e316c6
BanSolo03 <nikibano@gmail.com>
2023-10-03 15:34:31 +0200

    Aggiunto prototipo di login (credenziali indicate nel metodo "Authenticate" nel file "LoginPage.xaml.cs")

--- View/LoginPage.xaml.cs
+++ View/LoginPage.xaml.cs
@@ -6,19 +6,43 @@ namespace FinecoBankMobile.View;
 
 public partial class LoginPage : ContentPage
 {
-
     public LoginPage()
     {
         InitializeComponent();
     }
 
+    private async void Login_Clicked(object sender, EventArgs e)
+    {
+        string email = EmailEntry.Text;
+        string password = PasswordEntry.Text;
+
+        if (Authenticate(email, password))
+        {
+            await DisplayAlert("SUCCESS!", "Successfully logged in!", "OK");
+            EmailEntry.Text = "";
+            PasswordEntry.Text = "";
+            await Shell.Current.GoToAsync($"{nameof(HomePage)}");
+        }
+        else
+        {
+            await DisplayAlert("LOGIN FAILED", "Login failed. Check your credentials and try again", "OK");
+            EmailEntry.Text = "";
+            PasswordEntry.Text = "";
+        };
+    }
+    static bool Authenticate(string email, string password)
+    {
+        if (email == "luca.giovannini@gmail.com"  && password == "Tailwind1234")
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
     private async void Registration_Clicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync($"{nameof(RegisterPage)}");
-    }
-    private async void Login_Clicked(object sender, EventArgs e)  // Aggiungere controllo utente che restituisca in caso un messaggio di errore
-    {
-        await Shell.Current.GoToAsync($"{nameof(HomePage)}");
     }
 }
 
