29dd0186fd62bb902458ccd243409936a228afd8
BanSolo03 <nikibano@gmail.com>
2023-10-04 17:34:02 +0200

    DetailPage Collegata al Tap delle Operazioni

--- View/OperationsPage.xaml.cs
+++ View/OperationsPage.xaml.cs
@@ -4,6 +4,16 @@ public partial class OperationsPage : ContentPage
 {
 	public OperationsPage()
 	{
-        InitializeComponent();
+		InitializeComponent();
 	}
+
+	private async void Operation_Tapped(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync(nameof(DetailPage));
+	}
+
+    private void TextCell_Tapped(object sender, EventArgs e)
+    {
+
+    }
 }
